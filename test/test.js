996fcf0f8e1848abf2df521b605aaec2fc7fd430
David <david.ershag@gmail.com>
2014-02-10 20:21:12 +0100

    Fixed async tests callback

--- test/test.js
+++ test/test.js
@@ -8,29 +8,32 @@ function makefn(count) {
         if (++times === count) {
             done(null, times);
         } else {
-            done(new Error());
+            done(times);
         }
     }
 }
 
 describe('events', function () {
-    it('should succeed after executed three times', function () {
+    it('should succeed after executed three times', function (done) {
         retry(makefn(3)).on('error', function (err) {
             assert.fail();
         }).on('done', function (result) {
             assert.equal(3, result);
+            done();
         });
     });
-    it('should succeed after executed five times', function () {
+    it('should succeed after executed five times', function (done) {
         retry(makefn(5), 5).on('error', function (err) {
             assert.fail();
         }).on('done', function (result) {
             assert.equal(5, result);
+            done();
         });
     });
-    it('should fail after executed three times', function () {
+    it('should fail after executed three times', function (done) {
         retry(makefn(5)).on('error', function (err) {
-            assert.notEqual(null, err);
+            assert.equal(3, err);
+            done();
         }).on('done', function (result) {
             assert.fail();
         });
@@ -38,33 +41,38 @@ describe('events', function () {
 });
 
 describe("callbacks", function() {
-    it('should succeed after executed once', function () {
+    it('should succeed after executed once', function (done) {
         retry(makefn(1), function (err, result) {
             assert.equal(null, err);
             assert.equal(1, result);
+            done();
         });
     });
-    it('should succeed after executed twice', function () {
+    it('should succeed after executed twice', function (done) {
         retry(makefn(2), function (err, result) {
             assert.equal(null, err);
             assert.equal(2, result);
+            done();
         });
     });
-    it('should succedd after executed five times', function () {
+    it('should succedd after executed five times', function (done) {
         retry(makefn(5), function (err, result) {
             assert.equal(null, err);
             assert.equal(5, result);
+            done();
         }, 5);
-    });it('should give error after executed three times', function () {
+    });it('should give error after executed three times', function (done) {
         retry(makefn(999), function (err, result) {
-            assert.notEqual(null, err);
+            assert.equal(3, err)
             assert.equal(null, result);
+            done();
         });
     });
-    it('should give error after executed five times', function () {
+    it('should give error after executed five times', function (done) {
         retry(makefn(999), function (err, result) {
-            assert.notEqual(null, err);
+            assert.equal(5, err);
             assert.equal(null, result);
+            done();
         }, 5);
     });
 });
