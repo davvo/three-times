e0ed8fb86cf5c575d30f40ae6d9630e72df342b1
David <david.ershag@gmail.com>
2014-02-10 22:59:07 +0100

    Updated example

--- example/numbers.js
+++ example/numbers.js
@@ -1,20 +1,27 @@
-var retry = require('three-times');
+var retry = require('../lib/index');
 
-var count = 0;
-
-function roll(done) {
-    count++;
-    var dice = 1 + Math.floor(6 * Math.random());
-    if (dice !== 6) {
-        return done(new Error());
-    }
-    done(null, count);
+function rollDice() {
+    return 1 + Math.floor(6 * Math.random());
 }
 
-retry(roll, function (err, res) {
+function gamble(done) {
+    var dice1 = rollDice(), 
+        dice2 = rollDice();
+    if (dice1 === 1 && dice2 === 1) {
+        done(null, 'Snake Eyes!');
+    } else if (dice1 === 4 && dice2 === 5) {
+        done(null, 'Jesse James!')
+    } else if (dice1 === 6 && dice2 === 6) {
+        done(null, 'Boxcars!');
+    } else {
+        done(new Error());
+    }
+}
+
+retry(gamble, function (err, res) {
     if (err) {
         console.log("No luck :-(");
     } else {
-        console.log("A six on attempt " + res);
+        console.log(res);
     }
-});
+}, 5);
